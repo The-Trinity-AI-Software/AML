cbb4ccd03c33aa20d8a4c665b2f1f2eb0889cb1c
sumanthg201994 <sumanthg@thetrinitysoftware.com>
2025-05-20 09:49:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def download_excel():
     return send_from_directory(path, "predictions.xlsx", as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=7002)
+    app.run(host='0.0.0.0', debug=True, port=9097)
